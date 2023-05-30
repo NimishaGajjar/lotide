eea30b3dd3e12e9e733596267da2897113395f54
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-05-30 18:50:58 -0400

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: Arrays are equal.`);
+
+  } else {
+    console.log(`❌❌❌ Assertion Failed: Arrays are NOT equal.`);
+  }
+};
+
+module.exports = assertArraysEqual;
