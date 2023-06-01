97a084ac9d38d104dc1586e267125d100c9d1db4
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-01 18:53:11 -0400

    Implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,52 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: Arrays are equal.`);
+
+  } else {
+    console.log(`❌❌❌ Assertion Failed: Arrays are NOT equal.`);
+  }
+};
+
+// ACTUAL FUNCTION
+const takeUntil = (array, callback) => {
+  let output = [];
+
+  for (const element of array) {
+    if (callback(element)) {
+      break;
+    } else {
+      output.push(element);
+    }
+  }
+
+  return output;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const sentence = ['Hi', 'there', 'Mr', 'Person', 'you', 'are', 'silly'];
+const result2 = takeUntil(sentence, word => word === 'you');
+
+assertArraysEqual(result2, ['Hi', 'there', 'Mr', 'Person']);
