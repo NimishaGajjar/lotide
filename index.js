51ee3f4776d1a33c9fa7982fa3ba39029e193aef
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-05 18:40:51 -0400

    Implement Object Shorthand-index.js

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
+// const head = require('./head');
+// const tail = require('./tail');
+// const middle = require('./middle');
+
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle
+// };
+
+// // console.log(head([1, 2, 3])); // => 1
+// // console.log(tail([1, 2, 3])); // => [2, 3]
+
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
 const head = require('./head');
-const tail = require('./tail');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
-
 // console.log(head([1, 2, 3])); // => 1
 // console.log(tail([1, 2, 3])); // => [2, 3]
+// console.log(middle([1, 2, 3]));
