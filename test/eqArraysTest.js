cb7c181f1bb61b7a3067f0422a7f35a91929d4e3
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-06-05 17:39:30 -0400

    using arraytest mocha and chai

---
+++ test/eqArraysTest.js
@@ -0,0 +1,17 @@
+const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+
+describe("#eqArrays", () => {
+  it("returns 'true' for same arrays", () => {
+    assert.strictEqual(eqArrays([1, 7, 3], [1, 7, 3]), true);
+  });
+  it("returns 'false' for different arrays", () => {
+    assert.strictEqual(eqArrays([1, 5, 3], [1, 7, 3]), false);
+  });
+  it("returns 'true' for same nested-arrays", () => {
+    assert.strictEqual(eqArrays([1, [2, 3]], [1, [2, 3]]), true);
+  });
+  it("returns 'true' for same mixed number-string nested-arrays", () => {
+    assert.strictEqual(eqArrays(["1", [[[2], ["3"]]]], ["1", [[[2], ["3"]]]]), true);
+  });
+});
